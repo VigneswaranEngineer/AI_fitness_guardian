edb92b677eda5132ebde3b008c68ee5c13b2c230
VigneswaranEngineer <96531172+VigneswaranEngineer@users.noreply.github.com>
2024-02-04 18:37:46 +0530

    Update muscle.c

--- muscle.c
+++ muscle.c
@@ -1,21 +1,13 @@
 const int muscleSensorPin = A0; // Analog pin where the muscle sensor is connected
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication for debugging
+  Serial.begin(115200); // Initialize serial communication for debugging
+  while(!Serial){}
 }
 
 void loop() {
   // Read analog value from muscle sensor
   int muscleSensorValue = analogRead(muscleSensorPin);
-
-  // Print the value to the serial monitor
-  Serial.println("Muscle Sensor Reading: " + String(muscleSensorValue));
-
-  // Send the value to the Serial Plotter
-  Serial.print("M ");
-  Serial.println(muscleSensorValue);
-
-  // Add your logic for processing and controlling devices based on the sensor reading
   
   delay(1000); // Wait for a short duration before the next reading
 }
